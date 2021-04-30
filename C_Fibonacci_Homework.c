a836e36524216d99b864d53a7c2521e7529ae7d3
elizabethvickery <78845148+elizabethvickery@users.noreply.github.com>
2021-04-30 11:28:08 +0400

    Update C_Fibonacci_Homework.c

--- C_Fibonacci_Homework.c
+++ C_Fibonacci_Homework.c
@@ -1,21 +1,35 @@
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
 int main()
 {
-   int a=0, b=1, age, c, sum=0;
+   int f1=0, f2=1, age, f3, sum=0;
 
    printf("Enter your age: ");
    scanf("%d",&age);
 
-   for(int c=0; c<age; c++)
+   for(int f3=0; f3<age; f3++)
    {
-      sum += a;
-      c = a + b; //next element
-      a = b;
-      b = c;
-
+      sum += f1;
+      f3 = f1 + f2; //next element
+      f1 = f2;
+      f2 = f3;
    }
 
    printf("Sum = %d", sum);
 
    return 0;
+
+long long convert(int sum)
+      {
+        long long bin = 0;
+        int rem, i = 1, step = 1;
+        while (sum != 0) {
+                rem = sum % 2;
+                printf("Step %d: %d/2, Remainder = %d, Quotient = %d\sum", step++, sum, rem, sum / 2);
+                sum /= 2;
+                bin += rem * i;
+                i *= 10;
+        }
+        return bin;
 }
+
